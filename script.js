ac0515b09a38f1ef903f123fc899919c610c17fb
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-02-28 11:21:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,134 @@
-const images = document.querySelectorAll(".image");
-const form = document.querySelector(".form");
-const dice = document.querySelector(".dice");
-const coupon = document.querySelector(".coupon");
-const congrats = document.querySelector(".congrats");
-const rollDiceButton = document.querySelector("#roll-dice");
-let clickCount = 0;
-let total = 0;
-let attempts = 0;
+const img1El = document.getElementById("img1");
+console.log("img1El");
+const img2El = document.getElementById("img2");
+const img3El = document.getElementById("img3");
+const img4El = document.getElementById("img4");
+const userDetailsEl = document.querySelector(".user-details");
+const formContainerEl = document.querySelector(".form-container");
+let diceEl = document.querySelector("#dice");
+const btn = document.getElementById("btn");
+let name;
+let username;
+let email;
 
-images[0].addEventListener("click", function() {
-  if (clickCount === 0) {
-    form.style.display = "block";
-    images[0].style.pointerEvents = "none";
-    clickCount++;
+// Image 1 handler
+img1El.addEventListener(
+  "click",
+  () => {
+    formContainerEl.classList.remove("hide");
+    Name = formContainerEl.querySelector("#name").value;
+    username = formContainerEl.querySelector("#username").value;
+    email = formContainerEl.querySelector("#email").value;
+    userDetailsEl.innerHTML = "";
+  },
+  { once: true }
+);
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  Name = formContainerEl.querySelector("#name").value;
+  username = formContainerEl.querySelector("#username").value;
+  email = formContainerEl.querySelector("#email").value;
+  if (!email.includes("@")) {
+    alert("Please enter a proper email id");
+  } else if (Name === "" || username === "" || email === "") {
+    alert("Please fill form details Properly ");
+  } else {
+    formContainerEl.classList.add("hide");
+    formContainerEl.querySelector("#name").value = "";
+    formContainerEl.querySelector("#username").value = "";
+    formContainerEl.querySelector("#email").value = "";
+    userDetailsEl.innerHTML = `
+    <h1> Please Click on Second Image to view your data </h1>`;
+    hanldeImg2();
   }
 });
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  const name = document.querySelector("#name").value;
-  const username = document.querySelector("#username").value;
-  form.style.display = "none";
-  images[1].style.pointerEvents = "auto";
-  images[1].addEventListener("click", function() {
-    alert(`Name: ${name}\nUsername: ${username}`);
-    images[1].style.pointerEvents = "none";
-    images[2].style.pointerEvents = "auto";
+// Image 2 handler
+function hanldeImg2() {
+  img2El.addEventListener("click", function a() {
+    userDetailsEl.innerHTML = `
+    <h1>Please Click on Third Image to view the game</h1>  
+    <h3> Username: ${username} </h3>
+    <h3>name: ${Name} </h3>
+      `;
+    handelImg3();
+    img2El.removeEventListener("click", a);
   });
-});
+}
 
-images[2].addEventListener("click", function() {
-  dice.style.display = "block";
-  images[2].style.pointerEvents = "none";
-});
+// Varaibles for dice
+let sum = 0;
+let count = 0;
+let times = 0;
 
-rollDiceButton.addEventListener("click", function() {
-  const result = Math.floor(Math.random() * 6) + 1;
-  total += result;
-  document.querySelector("#result").textContent = `You rolled a ${result}. Total: ${total}`;
-  attempts++;
-  if (attempts === 3) {
-    dice.style.display = "none";
-    if (total > 10) {
-      images[3].style.pointerEvents = "auto";
-    } else {
-      if (attempts === 2) {
-        alert("Bad luck");
-      } else {
-        alert("Please try again");
-        attempts = 0;
-        total = 0;
-      }
-    }
+let sum1 = 0;
+let count1 = 3;
+let chances = 1;
+
+//Dice handler
+diceEl.addEventListener("click", handleClick);
+function handleClick() {
+  if (count1 > 0) {
+    let ran;
+    ran = Math.floor(Math.random() * 6) + 1;
+    sum1 += ran;
+    count1--;
+    document.querySelector(".p").innerHTML = `
+    The sum is ${sum1}`;
+    // console.log(sum1)
   }
-});
+  if (sum1 > 10 && count1 === 0) {
+    document.querySelector(".p").innerHTML = `
+    The sum is ${sum1} you can click img 4`;
+    alert("congrat sum is greater than 10");
+    diceEl.classList.toggle("hide");
+    img3El.removeEventListener("click", img3Func);
 
-images[3].addEventListener("click", function() {
-  coupon.style.display = "block";
-  const couponCode = Math.floor(Math.random() * 1000000000000).toString();
-  document.querySelector(".coupon p").textContent = `Your coupon code is: ${couponCode}`;
-  images[3].style.pointerEvents = "none";
-  congrats.style.display = "block";
-});
+    img4El.addEventListener("click", hanldeImg4, { once: true });
+  } else if (sum1 <= 10 && chances > 0 && count1 === 0) {
+    count1 = 3;
+    sum1 = 0;
+    chances--;
+    document.querySelector(".p").innerHTML = `
+    The sum is ${sum1}`;
+    diceEl.classList.toggle("hide");
+    alert("sum is below 10 please try again by clicking image 3");
+    handelImg3();
+  } else if (sum1 <= 10 && chances === 0 && count1 === 0) {
+    alert("Bad Luck cannot try more than 2 time");
+    document.querySelector(".p").innerHTML = `
+    Bad Luck Cannot try more`;
+    diceEl.classList.toggle("hide");
+
+    img3El.removeEventListener("click", img3Func);
+  }
+}
+
+
+// Image 3 handler
+function handelImg3() {
+  img3El.addEventListener("click", img3Func, { once: true });
+}
+
+function img3Func() {
+  userDetailsEl.innerHTML = "";
+  diceEl.classList.remove("hide");
+}
+// Generate random number between 1-6 for the dice
+function getRandom() {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  return randomNumber;
+}
+
+// Image 4 handler
+
+function hanldeImg4() {
+  let random = crypto.randomUUID();
+  random = random.slice(-12, -1);
+  userDetailsEl.innerHTML = `
+      <h1>The coupon code  ${"#" + random}</h1>
+  `;
+  document.querySelector(".p").innerHTML = `
+      <img src="images/congratulations.png" id="img4" style=" align: left; 
+      margin-right:700px;"  />
+      `;
+}
